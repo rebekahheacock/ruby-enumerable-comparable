2fea97f8f53a01c40c9901f1f200b02e30b51936
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-16 11:18:52 -0400

    define <=> in Card

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,20 @@
 # A simple representation of a playing card.
 class Card
+  # static definitions
   SUITS = %w(C D H S).freeze
   RANKS = [(2..10).to_a, %w(J Q K A)].flatten.freeze
+
+  def <=>(other)
+    suit_comparison = SUITS.index(suit) <=> SUITS.index(other.suit)
+    rank_comparison = RANKS.index(rank) <=> RANKS.index(other.rank)
+
+    # sort first by suit, then by rank
+    if suit_comparison.zero?
+      rank_comparison
+    else
+      suit_comparison
+    end
+  end
 
   attr_reader :suit, :rank
 
