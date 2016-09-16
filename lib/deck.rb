602a13b745e5ab78886ce5223dd0a6d71bf31bc5
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-16 11:54:23 -0400

    change @deck to @storage in Deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,36 +2,36 @@ require_relative 'card'
 
 # A simple representation of a deck of playing cards
 class Deck
-  # deck is a getter method that returns @deck
-  attr_reader :deck
+  # storage is a getter method that returns @storage
+  attr_reader :storage
 
-  # deck is a PRIVATE getter method that can only be used inside this class
-  private :deck
+  # storage is a PRIVATE getter method that can only be used inside this class
+  private :storage
 
   def initialize
-    # @deck is an array
-    @deck = Card::SUITS.map do |suit|
+    # @storage is an array
+    @storage = Card::SUITS.map do |suit|
       Card::RANKS.map { |rank| Card.new(rank, suit) }
     end.flatten
   end
 
   # swap front and back somewhere in the middle third.
   def cut
-    count = deck.length
+    count = storage.length
     random = Random.rand(count / 3)
     cut_point = (count / 3 + random)
-    deck.replace deck.slice(cut_point, count - cut_point) +
-                 deck.slice(0, cut_point)
+    storage.replace storage.slice(cut_point, count - cut_point) +
+                    storage.slice(0, cut_point)
     self
   end
 
   def draw
-    # deck here is a METHOD, defined by attr_reader, that returns @deck
-    deck.shift
+    # storage here is a METHOD, defined by attr_reader, that returns @storage
+    storage.shift
   end
 
   def shuffle
-    deck.shuffle!
+    storage.shuffle!
     self
   end
 
