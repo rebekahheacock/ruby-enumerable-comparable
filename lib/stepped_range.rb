c41d552f05595f5e62cca4884a5545ebcd803e4a
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-16 13:31:33 -0400

    define Stepped Range class and make Enumerable

--- lib/stepped_range.rb
+++ lib/stepped_range.rb
@@ -1,3 +1,27 @@
 #
 class SteppedRange
+  include Enumerable
+
+  def each
+    current = start
+    while current <= stop
+      # yield is kind of like return
+      # used for enumeration
+      yield current
+      current += step
+    end
+  end
+
+  attr_reader :start, :stop, :step
+  private :start, :stop, :step
+
+  def initialize(start, stop, step)
+    # this is the same as
+    # @start, @stop, @step = start, stop, step
+    # which is called "parallel assignment"
+    # shouldn't use that
+    @start = start
+    @stop = stop
+    @step = step
+  end
 end
